f61d91c57caed2b898a00524af625b88a340547a
Venia57 <Veniaaurilia57@gmail.com>
2024-05-22 14:12:57 +0700

    grid layout

---
+++ app/src/main/java/com/irfin_falah_212102390/gridlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.irfin_falah_212102390.gridlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
